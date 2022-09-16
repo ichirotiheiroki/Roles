3c5415198f1d4902b93923539630142c8d13856d
Ichiro Tiheiroki <74300287+ichirotiheiroki@users.noreply.github.com>
2022-09-16 18:05:32 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 var headers = ["H1", "H2", "H3", "H4", "H5", "H6"];
-
 $(".accord").click(function (e) {
   var target = e.target,
     name = target.nodeName.toUpperCase();
-
   if ($.inArray(name, headers) > -1) {
     var subItem = $(target).next();
-
-    //slideUp all elements (except target) at current depth or greater
     var depth = $(subItem).parents().length;
     var allAtDepth = $(".accord p, .accord div").filter(function () {
       if ($(this).parents().length >= depth && this !== subItem.get(0)) {
@@ -15,8 +11,6 @@ $(".accord").click(function (e) {
       }
     });
     $(allAtDepth).slideUp("fast");
-
-    //slideToggle target content and adjust bottom border if necessary
     subItem.slideToggle("fast", function () {
       $(".accord :visible:last").css("border-radius", "0 0 10px 10px");
     });
